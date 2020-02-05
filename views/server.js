980d0a83739d1755b87cf694a0b8508f33209145
PanosChristopoulos <56166480+PanosChristopoulos@users.noreply.github.com>
2020-02-05 04:40:23 +0200

    Add files via upload

---
+++ views/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+var username = document.getElementById("fname").value;
+var email = document.getElementById("lname").value;
+var subject = document.getElementById("subject").value;
+
+const nodemailer = require('nodemailer');
+
+//step1
+
+let transporter = nodemailer.createTransport({
+        service: 'gmail',
+            auth:{
+                user: 'jekiriquestions@gmail.com',
+                pass: 'BenficaMacau41'
+
+}
+});
+
+
+let mailOptions = {
+    from: 'jekiriquestions@gmail.com',
+    to:'jekiricrowdsourcing@gmail.com',
+    subject: 'Νέο μήνυμα της εφαρμογής Jekiri Crowdsourcing',
+    text: 'Ο χρήστης '+ username+ 'με email'+email + 'έγραψε'+subject
+}
+
+transporter.sendMail(mailOptions, function(err,data){
+    if(err){
+        console.log('Error occurs');
+    } else {console.log('Mail sent')};
+
+
+
+})
