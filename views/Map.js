91ce241325a7a257317dc3920c9d812e8f1383c1
PanosChristopoulos <56166480+PanosChristopoulos@users.noreply.github.com>
2020-02-05 04:39:50 +0200

    Add files via upload

--- views/Map.js
+++ views/Map.js
@@ -1,5 +1,5 @@
 
-function createMap(){
+function createMap(testData2){
 let mymap = L.map('mapid');
 
 let url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -7,6 +7,26 @@ let url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 let osm= new L.TileLayer(url);
 	mymap.addLayer(osm);
 	mymap.setView([38.246242, 21.7350847],16);
+
+let cfg = {
+  "radius": 40,
+  "maxOpacity": 0.8,
+  
+  "scaleRadius": false,
+ 
+   "useLocalExtrema": false,
+ 
+  latField: 'lat',
+ 
+  lngField: 'lng',
+ 
+  valueField: 'count'
+};
+
+let heatmapLayer =  new HeatmapOverlay(cfg);
+
+mymap.addLayer(heatmapLayer);
+heatmapLayer.setData(testData2);
 
 
 }
